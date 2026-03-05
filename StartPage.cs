a0b3a54eb43c51960cd5c030d53569a2d075a148
opilane <opilane@HP-CZC2349HTB>
2026-03-05 12:32:50 +0200

    Add project files.

---
+++ StartPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praktilised_ulesanded_Failitootlus_ja_Listid
+{
+    internal class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            bool tööta = true;
+            while (tööta)
+            {
+                Console.WriteLine("1.  Pinginaabrid");
+                Console.WriteLine("2.  Kogu menüü kuvamine");
+                Console.WriteLine("3.  Koostisosade muutmine nimekirjas");
+
+                string valik = Console.ReadLine();
+                Console.WriteLine();
+                switch (valik)
+                {
+                    case "1":
+                        Ulesanded.Lemmiktoit();
+                        break;
+                    case "2":
+                        Ulesanded.KoguMenu();
+                        break;
+                    case "3":
+                        Ulesanded.Koostisosade_muutmine();
+                        break;
+
+                }
+        }
+    }
+}
---
+++ Ulesanded.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text;
+
+
+
+    public class Ulesanded
+    {
+         
+        public static void Lemmiktoit()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+                StreamWriter text = new StreamWriter(path, true);
+                Console.WriteLine("Sisesta ühte Itaalia toidu nime:");
+                string lause = Console.ReadLine();
+                text.WriteLine(lause);
+                text.Close();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Mingi viga failiga");
+            }
+            
+        }
+        public static void KoguMenu()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+                StreamReader text = new StreamReader(path);
+                string laused = text.ReadToEnd();
+                text.Close();
+                Console.WriteLine(laused);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Mingi viga failiga, ei saa faili lugeda");
+            }
+        }
+        public static List<string> Koostisosade_muutmine()
+        {
+            List<string> Koostisosad = new List<string>();
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+                foreach (string rida in File.ReadAllLines(path))
+                {
+                    Koostisosad.Add(rida);
+                }
+               
+                
+                if (Koostisosad.Count > 0)
+                    Koostisosad[0] = "Kvaliteetne oliiviõli";
+                
+                Koostisosad.Remove("Ketšup");
+                foreach (string item in Koostisosad)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Viga failiga!");
+            }
+            return Koostisosad;
+        }
+
+
+
+
+    }
+
+
+
